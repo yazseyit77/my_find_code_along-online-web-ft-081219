b04ef41dffcd0956633db145bf0ba53867d6334e
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-17 19:58:52 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,10 +4,8 @@ def my_find(collection)
 
   i = 0
   while i < collection.length
-    if yield(collection[i])
-      return collection[i]
-    end
-    i = i + 1
+      return collection[i] if yield(collection[i])
+      i = i + 1
       binding.pry
   end
 end
